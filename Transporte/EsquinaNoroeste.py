f55cc5ba13a72a0020f11eb1aba20c579b71aa90
kevin <castillokevin37@gmail.com>
2020-05-07 21:17:54 -0500

    Se balanceo problema

--- Transporte/EsquinaNoroeste.py
+++ Transporte/EsquinaNoroeste.py
@@ -3,28 +3,41 @@ import numpy as np
 def EsqNor(oferta,demanda,costos):
     #Verificamos que el problema sea balanceado
     if sum(oferta) != sum(demanda):
+        print("Problema no balanceado\n")
         if sum(oferta) > sum(demanda):
-            ## Se agrega una nueva columna con ceros
             demanda = np.append(demanda,(sum(oferta)-sum(demanda)))
+            ## Se agrega  la variable artificial como columna
+            var_artificial = np.zeros(costos.shape[0])
+            costos = np.column_stack((costos,var_artificial))
+            print("Se balanceara\n")
+
         else:
-            ## Se agraga nueva fila de ceros
             oferta = np.append(oferta,(sum(demanda)-sum(oferta)))
-        
-        print(oferta)
-        print(demanda)
+            ## Se agrega la variable artificial como fila
+            var_artificial = np.zeros(costos.shape[1])
+            costos = np.row_stack((costos,var_artificial))
+            print("Se balanceara\n")
+    else:
+        print("Problema balanceado\n")
+    
+
+    print("Oferta:" ,oferta)
+    print("Demanda: ",demanda)
+    print("Costos:\n", costos)
 
 
 
 
+demanda = np.array([250,250,100])
+oferta = np.array([100,200,150,100])
 
-oferta= np.array([105, 125, 70,20,102])
-demanda = np.array([80, 65, 70, 85])
-
-costos = np.array([[9., 10., 13., 17.],
-                          [7., 8., 14., 16.],
-                          [20., 14., 8., 14.]])
+costos = np.array([[50, 78, 85, 20.],
+                          [40,35, 100, 90],
+                          [55, 25, 60, 80]])
 
 EsqNor(oferta,demanda,costos)
+
+
 
 
 
